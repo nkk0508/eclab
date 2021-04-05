5d6c1225559ba92b1c6e577ddaeb7f92dbe26b1c
nkk0508 <keita19920508@gmail.com>
2021-04-06 00:46:21 +0900

    first commit

---
+++ src/main/java/com/eclab/eclab/EclabApplication.java
@@ -0,0 +1,13 @@
+package com.eclab.eclab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclabApplication.class, args);
+	}
+
+}
